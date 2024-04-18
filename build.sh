340a1041ac6fd3f0bfe1a89204cb42e905ec00ef
oberdfr <alebennella@gmail.com>
2024-04-18 12:10:54 +0200

    build: add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+PATH="/home/oberdfr/toolchains/proton-clang/bin:${PATH}" \
+make -j16 \
+ 	ARCH=arm64 \
+	CC=clang \
+ 	LD=ld.lld \
+ 	LLVM=1 \
+ 	LLVM_IAS=1 \
+ 	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=arm-linux-gnueabi- $@
